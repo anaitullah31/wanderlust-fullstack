e8216782fe8e861b5ae3a4a61c04131cb7424f27
anaitullah31 <anaitullah198@gmail.com>
2026-05-13 13:55:14 +0800

    Setup better auth for authentication and remove .env for tracking on backend

---
+++ wanderlust-client/lib/auth.js
@@ -0,0 +1,18 @@
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsynxg9.mongodb.net/?appName=Cluster0`,
+);
+const db = client.db("wanderlust");
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
+});
