0049538bface0ee7960083dbcb1cf5fa945d9930
anaitullah31 <anaitullah198@gmail.com>
2026-05-13 14:33:07 +0800

    Implement email and password signup

--- wanderlust-client/app/signup/page.js
+++ wanderlust-client/app/signup/page.js
@@ -1,8 +1,41 @@
+"use client";
 import Link from "next/link";
-import { Lock, Mail, User } from "lucide-react";
+import { Image, Lock, Mail, User } from "lucide-react";
 import GoogleLoginButton from "../components/GoogleLoginButton";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const SignUpPage = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+
+    if (loading) return;
+
+    setLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+    const user = Object.fromEntries(formData.entries());
+
+    const { data, error } = await authClient.signUp.email({
+      email: user.email,
+      password: user.password,
+      name: user.name,
+      image: user.image,
+    });
+
+    if (error) {
+      setLoading(false);
+      alert(error.message);
+      return;
+    }
+
+    router.push("/");
+  };
+
   return (
     <div className=" bg-gray-50">
       <section className="py-16">
@@ -16,7 +49,10 @@ const SignUpPage = () => {
             </p>
           </div>
 
-          <form className="bg-white border border-gray-200 shadow-md p-8 space-y-5">
+          <form
+            onSubmit={handleSignupSubmit}
+            className="bg-white border border-gray-200 shadow-md p-8 space-y-5"
+          >
             <div>
               <label className="text-sm font-medium text-black">
                 Full Name
@@ -28,6 +64,7 @@ const SignUpPage = () => {
                 />
                 <input
                   type="text"
+                  name="name"
                   placeholder="Enter your name"
                   className="w-full border border-gray-200 bg-gray-50 py-3 pl-10 pr-4 text-sm outline-none"
                 />
@@ -45,7 +82,27 @@ const SignUpPage = () => {
                 />
                 <input
                   type="email"
+                  name="email"
                   placeholder="Enter your email"
+                  className="w-full border border-gray-200 bg-gray-50 py-3 pl-10 pr-4 text-sm outline-none"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="text-sm font-medium text-black">
+                Image URL
+              </label>
+              <div className="relative mt-2">
+                <Image
+                  alt=""
+                  className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+                  size={16}
+                />
+                <input
+                  type="url"
+                  name="image"
+                  placeholder="Enter your image url"
                   className="w-full border border-gray-200 bg-gray-50 py-3 pl-10 pr-4 text-sm outline-none"
                 />
               </div>
@@ -60,13 +117,14 @@ const SignUpPage = () => {
                 />
                 <input
                   type="password"
+                  name="password"
                   placeholder="Create a password"
                   className="w-full border border-gray-200 bg-gray-50 py-3 pl-10 pr-4 text-sm outline-none"
                 />
               </div>
             </div>
 
-            <div>
+            {/* <div>
               <label className="text-sm font-medium text-black">
                 Confirm Password
               </label>
@@ -81,13 +139,21 @@ const SignUpPage = () => {
                   className="w-full border border-gray-200 bg-gray-50 py-3 pl-10 pr-4 text-sm outline-none"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <button
+            {/* <button
               type="submit"
               className="w-full cursor-pointer bg-cyan-600 py-3 text-sm font-medium text-white hover:bg-cyan-700"
             >
               Create Account
+            </button> */}
+
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-full bg-cyan-600 py-3 ${loading ? "" : "cursor-pointer"} text-sm font-medium text-white hover:bg-cyan-700 disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-cyan-600`}
+            >
+              {loading ? "Creating Account..." : "Create Account"}
             </button>
 
             <div className="flex items-center gap-3">
